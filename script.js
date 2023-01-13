2eec419b0f98fbd1df7adc86bb81a2f971ff915c
Reetu Raj Chauhan <111192512+rrc-IIESTs@users.noreply.github.com>
2023-01-13 15:18:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+str = str.toLowerCase();
+let x = str.length;
+for(let i=0;i<x/2;i++){
+	if(str.charAt(i)!=str.charAt(x-i)){
+		return false;
+	}
+}
+return true;
 }
 module.exports = palindrome
