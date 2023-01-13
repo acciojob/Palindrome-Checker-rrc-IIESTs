20b6305326f615437027bb666c25003e73dc6a93
Reetu Raj Chauhan <111192512+rrc-IIESTs@users.noreply.github.com>
2023-01-13 16:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function palindrome(str){
 str = str.toLowerCase();
 let x = str.length;
 for(let i=0;i<x/2;i++){
-	if(str.charAt(i)!=str.charAt(x-i)){
+	if(str.charAt(i)!=str.charAt(x-i-1)){
 		return false;
 	}
 }
